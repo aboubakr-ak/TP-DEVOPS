6a620b5cb36c731bf73f47eaf2e03ab322a9f248
ABOUBAKR <ha_akacha@esi.dz>
2024-04-25 16:26:19 +0200

    added delete

--- user.py
+++ user.py
@@ -50,4 +50,12 @@ def update_user(user_id: int, request: schemas.User, db: Session = Depends(get_d
     return {"message": "User updated successfully"}
 
 
-# delete an existing user:
+# Delete an existing user
+@route.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(user)
+    db.commit()
+    return {"message": "User deleted successfully"}
