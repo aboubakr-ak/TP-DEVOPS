cec7721c8ba98368057eb839a4cda332aebc125c
ABOUBAKR <ha_akacha@esi.dz>
2024-04-25 16:24:02 +0200

    updated POST user

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import Base, get_db
@@ -12,7 +12,9 @@ route = APIRouter()
 # Add new user
 @route.post("/users", response_model=schemas.User)
 def add_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name,
+    new_user = models.User(
+                           id=request.id,
+                           name=request.name,
                            birthday=request.birthday,
                            gender=request.gender,
                            email=request.email)
@@ -27,7 +29,14 @@ def get_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
 
 
-# Retrieve details for a specific user:
+# Retrieve details for a specific user
+@route.get("/users/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
 
 # Update an existing user:
 
